6fe93eccbe1e902ec7d4efff4f718ee76ef8aee8
Patil Bharath Sai <144615104+PATIL-BHARATH-SAI@users.noreply.github.com>
2024-07-05 00:04:54 +0530

    Add files via upload

---
+++ RightAngleTriangle.java
@@ -0,0 +1,17 @@
+public class RightAngleTriangle {
+    
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n) {
+        for (int row = 1; row <= n; row++) {
+            // for every row, run the col
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+}
